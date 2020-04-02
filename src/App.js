d92740e3002929c9ff9697d2200b67e4bbd32d87
Dhruvil Dave <dhruvil@gkmit.co>
2020-04-02 16:52:43 +0530

    flow of project updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 
 import React, { Component } from 'react';
-import PostForm from './post-form';
-import AllPost from './all-post';
+import PostComponent from './components';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="navbar">
-          <h2 className="center ">Post It</h2>
-        </div>
-        <PostForm />
-        <AllPost />
-      </div>
+      <PostComponent />
     );
   }
 }
